174a7932e7163655b1d0ed6e7d3a111d52d0e495
Allan Rizza <allanrizza.dev@gmail.com>
2025-01-26 20:37:59 -0300

    teste ValidaFaturamento

---
+++ Alura.Estacionamento.Testes/PatioTestes.cs
@@ -0,0 +1,33 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo
+            {
+                Proprietario = "Allan",
+                Tipo = TipoVeiculo.Automovel,
+                Cor = "Verde",
+                Modelo = "Fusca",
+                Placa = "ABC-9999"
+            };
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //assert
+            Assert.Equal(2, faturamento);
+        }
+    }
+}
