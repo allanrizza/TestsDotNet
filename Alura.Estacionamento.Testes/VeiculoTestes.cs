a19af0ae44aa57bcab0e74100c1d493b3995e219
Allan Rizza <allanrizza.dev@gmail.com>
2025-01-26 20:21:32 -0300

    Add project files.

---
+++ Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -0,0 +1,28 @@
+using Alura.Estacionamento.Modelos;
+using System;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class VeiculoTestes
+    {
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            //arrange
+            var veiculo = new Veiculo();
+            //act
+            veiculo.Acelerar(10);
+            //assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            var veiculo = new Veiculo();
+            veiculo.Frear(10);
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+    }
+}
